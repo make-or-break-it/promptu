03fff20e226dce6abfeb343bc920623ec20e3037
Berni Varga <berni.varga@deliveroo.co.uk>
2022-10-17 00:09:59 +0100

    Adding a simple version for the API

---
+++ api/Readme.md
@@ -0,0 +1,19 @@
+# API
+
+The API is written in GO.
+
+## Run
+
+```go run cmd/main.go```
+
+## Supported endpoints
+
+Post with:
+```
+curl -i -XPOST  -H "Content-Type: application/json" -d '{"user":"some_user","message":"hey there. i feel great!"}' http://localhost:8080/post
+```
+
+Get feed with:
+```
+curl -i http://localhost:8080/feed
+```
---
+++ api/cmd/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	cli "github.com/urfave/cli/v2"
+
+	"github.com/gorilla/mux"
+
+	"promptu/api/internal/handler"
+	"promptu/api/internal/storage"
+)
+
+func main() {
+	app := &cli.App{
+		Action: func(c *cli.Context) error {
+			store := storage.NewInMemoryStore()
+			return run(context.Background(), store)
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatalf("run failed: %s", err.Error())
+	}
+}
+
+func run(ctx context.Context, store handler.Store) error {
+
+	r := createRouter(store)
+	errCh := make(chan error, 1)
+
+	log.Print("Starting the server on port 8080")
+	srv := &http.Server{
+		Addr:         "localhost:8080",
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      middleware(r),
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			errCh <- err
+		}
+	}()
+	defer srv.Shutdown(ctx)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	select {
+	case <-sigChan:
+		return errors.New("shutting down due to received signal")
+	case err := <-errCh:
+		return err
+	}
+}
+
+func createRouter(store handler.Store) *mux.Router {
+	hndlr := handler.NewHandler(store)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/feed", hndlr.GetFeed).Methods(http.MethodGet)
+	r.HandleFunc("/post", hndlr.PostMessage).Methods(http.MethodPost)
+
+	return r
+}
+
+func middleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "application/json")
+		h.ServeHTTP(w, r)
+	})
+}

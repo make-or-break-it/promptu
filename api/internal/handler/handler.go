177f1bbf72488d7440c3fd56583990886c2505cc
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-10-24 23:01:18 +0000

    Making changes to API to support CORS

--- api/internal/handler/handler.go
+++ api/internal/handler/handler.go
@@ -24,23 +24,33 @@ func NewHandler(store Store) *Handler {
 
 func (h *Handler) GetFeed(w http.ResponseWriter, r *http.Request) {
 	feed, err := h.store.GetFeed(context.Background())
+
 	switch {
 	case err != nil:
 		writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	default:
 		jsonResp, err := json.MarshalIndent(feed, "", " ")
+
 		if err != nil {
 			writeError(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+
 		w.WriteHeader(http.StatusOK)
 		w.Write(jsonResp)
 	}
 }
 
 func (h *Handler) PostMessage(w http.ResponseWriter, r *http.Request) {
+	// Needed to support CORS: https://flaviocopes.com/golang-enable-cors/#:~:text=Handling%20pre%2Dflight%20OPTIONS%20requests
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
 	headerContentTtype := r.Header.Get("Content-Type")
+
 	if headerContentTtype != "application/json" {
 		writeError(w, "Content type is not application/json", http.StatusUnsupportedMediaType)
 		return
@@ -62,6 +72,7 @@ func (h *Handler) PostMessage(w http.ResponseWriter, r *http.Request) {
 		writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	w.WriteHeader(http.StatusCreated)
 }
 
@@ -69,6 +80,8 @@ func writeError(w http.ResponseWriter, message string, httpStatusCode int) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(httpStatusCode)
 	resp := make(map[string]string)
+
+	// TODO: add logging for when things go bad
 	resp["message"] = message
 	jsonResp, _ := json.Marshal(resp)
 	w.Write(jsonResp)

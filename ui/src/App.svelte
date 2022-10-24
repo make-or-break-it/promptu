f7e831c31bcac7aeb8528ea0528022eb5ee81e03
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-10-24 23:02:00 +0000

    Adding compatibility with existing APIs

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -2,14 +2,14 @@
   import Feed from "./lib/Feed.svelte";
   import Header from "./lib/Header.svelte";
   import PostPrompt from "./lib/PostPrompt.svelte";
+  import { posted } from "./stores/postManager"; // this needs to be replaced by an API
 
-  let timeToPost = false; // SWITCHABLE - based on API results
-  let posted = false; // SWITCHABLE - based on API results
+  let timeToPost = true; // SWITCHABLE - based on API results
 </script>
 
 <main>
   <Header />
-  {#if timeToPost && !posted}
+  {#if timeToPost && !$posted}
     <PostPrompt />
   {:else}
     <Feed />
--- ui/src/lib/Feed.svelte
+++ ui/src/lib/Feed.svelte
@@ -1,38 +1,34 @@
 <script>
-  let feed = [
-    // SWITCHABLE - based on API results
-    {
-      message: "Hey, eating some green beans and ham today",
-      user: "Cat in the Hat",
-    },
-    {
-      message: "Running out of things to wear",
-      user: "Mrs Piggy",
-    },
-    {
-      message: "What year is it?",
-      user: "alanparrish",
-    },
-    {
-      message: "Had a nice, long run!",
-      user: "Usain Bolt",
-    },
-    {
-      message: "Ready to bake some lovely cakes!",
-      user: "Mary Berry",
-    },
-  ];
+  const feedEndpoint = 'http://localhost:8080/feed'
+
+  const getFeed = (async () => {
+    let response = await fetch(feedEndpoint)
+    let feedContent = await response.json()
+
+    if ("posts" in feedContent) {
+      return feedContent["posts"]
+    } else {
+      return []
+    }
+  })()
 </script>
 
 <div class="feed">
-  {#each feed as post}
-    <div class="post">
-      <p class="message">{post.message}</p>
-      <p class="user">{post.user}</p>
-    </div>
-  {:else}
+  {#await getFeed}
     <p class="overlay">No posts yet!</p>
-  {/each}
+  {:then feed}
+    {#each feed as post}
+      <div class="post">
+        <p class="message">{post.message}</p>
+        <p class="user">{post.user}</p>
+      </div>
+    {:else}
+      <p class="overlay">No posts yet!</p>
+    {/each}
+  {:catch err}
+    <p class="overlay error">Error loading posts - computer says: <span class="error-code">{err.message}</span></p>
+    <p class="overlay error">Try reloading the page</p>
+  {/await}
 </div>
 
 <style>
@@ -63,5 +59,18 @@
 
   .overlay {
     text-align: center;
+    color: #2E282A;
+  }
+
+  .error {
+    color: #84110B;
+  } 
+
+  .error-code {
+    font-weight: normal;
+    background: #574C50;
+    color: white;
+    padding: 0.3em;
+    border-radius: 0.2em;
   }
 </style>

924ad264be667461a66ea76d6b35c7bc3d002718
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-11-21 14:13:00 +0000

    Replacing InMemory store client with MongoDB

--- api/internal/storage/storage.go
+++ api/internal/storage/storage.go
@@ -10,7 +10,3 @@ type Store interface {
 	GetFeed(ctx context.Context) (model.Feed, error)
 	PostMessage(ctx context.Context, post model.Post, createdAt time.Time) error
 }
-
-func NewStore(storeGenerator func() Store) Store {
-	return storeGenerator()
-}

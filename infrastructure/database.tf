9e1a084b1dedd63a69893633cf77f3b187d25c45
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-11-21 05:34:12 +0000

    Adding promptu user

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -1,5 +1,6 @@
 resource "mongodbatlas_cluster" "promptu-db" {
   project_id    = var.promptu_mongo_db_project_id
+
   name          = "promptu-db"
 
   provider_name               = "TENANT"
@@ -7,4 +8,16 @@ resource "mongodbatlas_cluster" "promptu-db" {
   provider_instance_size_name = "M0" # free tier DB
 
   provider_region_name = "EU_WEST_1"
+}
+
+resource "mongodbatlas_database_user" "prompt" {
+  project_id    = var.promptu_mongo_db_project_id
+
+  username = "promptu"
+  password = "thispasswordisnotreal" # can be changed without affecting the resource
+
+  roles {
+   role_name = "readWriteAnyDatabase"
+   database_name = "admin"
+  }
 }

40fb9a2e73af218db19d2a04137960fa13e2703d
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-11-21 05:01:43 +0000

    Updating free tier cluster details in Terraform

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -1,8 +1,10 @@
 resource "mongodbatlas_cluster" "promptu-db" {
   project_id    = var.promptu_mongo_db_project_id
   name          = "promptu-db"
-  provider_name = "AWS"
+
+  provider_name               = "TENANT"
+  backing_provider_name       = "AWS"
   provider_instance_size_name = "M0" # smallest free tier DB
 
-  cluster_type = "REPLICASET"
+  provider_region_name = "EU_WEST_2"
 }

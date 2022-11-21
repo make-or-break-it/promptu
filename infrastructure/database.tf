1273cd5f364781184a1779c29dfb3ddc2d71e5a9
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-11-21 05:18:14 +0000

    Updating MongoDBc comment

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -4,7 +4,7 @@ resource "mongodbatlas_cluster" "promptu-db" {
 
   provider_name               = "TENANT"
   backing_provider_name       = "AWS"
-  provider_instance_size_name = "M0" # smallest free tier DB
+  provider_instance_size_name = "M0" # free tier DB
 
   provider_region_name = "EU_WEST_1"
 }

8d31a9c8d6534140a648c0e2a15ab8e1dddcd055
Sanyia Saidova <sanyia.saidova@deliveroo.co.uk>
2023-02-08 17:50:21 +0000

    Testing using fly machines

--- infrastructure/apps.tf
+++ infrastructure/apps.tf
@@ -1,9 +1,18 @@
 resource "fly_app" "promptu" {
   name = var.promptu_fly_io_name_suffix == "" ? "promptu" : "promptu-${var.promptu_fly_io_name_suffix}"
-  org = var.promptu_fly_io_org_name
+  org  = var.promptu_fly_io_org_name
 }
 
 resource "fly_app" "promptu-api" {
   name = var.promptu_fly_io_name_suffix == "" ? "promptu-api" : "promptu-api-${var.promptu_fly_io_name_suffix}"
-  org = var.promptu_fly_io_org_name
+  org  = var.promptu_fly_io_org_name
+}
+
+resource "fly_machine" "example" {
+  app    = "test-app"
+  region = "iad"
+  image  = "ubuntu:latest"
+  env = {
+    testEnvVar = "testValue"
+  }
 }

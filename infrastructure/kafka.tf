75bddc434664983a94cefcff96ca6460efd4d526
BerniVarga <varga_bernadette@yahoo.com>
2023-02-12 18:35:59 +0000

    Add kafka setup to terraform

---
+++ infrastructure/kafka.tf
@@ -0,0 +1,35 @@
+resource "fly_app" "kafka" {
+  name = "kafka"
+  org = var.promptu_fly_io_org_name
+}
+
+resource "fly_app" "zookeeper" {
+  name = "zookeeper"
+  org = var.promptu_fly_io_org_name
+}
+
+resource "fly_volume" "kafka" {
+  name       = "kafka_data"
+  app        = "kafka"
+  size       = 1
+  region     = "lhr"
+  depends_on = [fly_app.kafka]
+}
+
+resource "fly_volume" "zookeeper" {
+  name       = "zookeeper_data"
+  app        = "zookeeper"
+  size       = 1
+  region     = "lhr"
+  depends_on = [fly_app.zookeeper]
+}
+
+resource "fly_ip" "kafkaIP" {
+  app  = "kafka"
+  type = "v6"
+}
+
+resource "fly_ip" "zookeeperIP" {
+  app  = "zookeeper"
+  type = "v6"
+}

3da89578b7d621f05ed8b890e9375856071bd74e
Sanyia Saidova <47756528+sansaid@users.noreply.github.com>
2023-02-12 22:50:17 +0000

    Fixing Terraform (#64)
    * Fixing dependancy chain of resources
    
    * Removing reliance on fly machines
    
    * Fixing dependency between app and IP assignment

--- infrastructure/zookeeper.tf
+++ infrastructure/zookeeper.tf
@@ -1,12 +1,11 @@
 resource "fly_volume" "zookeeper" {
-  name       = "zookeeper_data"
-  app        = "zookeeper"
-  size       = 1
-  region     = "lhr"
-  depends_on = [fly_app.zookeeper]
+  name   = "zookeeper_data"
+  app    = fly_app.zookeeper.name
+  size   = 1
+  region = "lhr"
 }
 
 resource "fly_ip" "zookeeper-ip" {
-  app  = "zookeeper"
+  app  = fly_app.zookeeper.name
   type = "v6"
 }

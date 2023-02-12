14fba5f0988db77cbc3d5f779d0f57148dd04265
Sanyia Saidova <sanyia.saidova@deliveroo.co.uk>
2023-02-12 22:26:22 +0000

    Fixing dependency between app and IP assignment

--- infrastructure/kafka.tf
+++ infrastructure/kafka.tf
@@ -1,9 +1,8 @@
 resource "fly_volume" "kafka" {
-  name       = "kafka_data"
-  app        = fly_app.kafka.name
-  size       = 1
-  region     = "lhr"
-  depends_on = [fly_app.kafka]
+  name   = "kafka_data"
+  app    = fly_app.kafka.name
+  size   = 1
+  region = "lhr"
 }
 
 resource "fly_ip" "kafka-ip" {
--- infrastructure/zookeeper.tf
+++ infrastructure/zookeeper.tf
@@ -1,9 +1,8 @@
 resource "fly_volume" "zookeeper" {
-  name       = "zookeeper_data"
-  app        = fly_app.zookeeper.name
-  size       = 1
-  region     = "lhr"
-  depends_on = [fly_app.zookeeper]
+  name   = "zookeeper_data"
+  app    = fly_app.zookeeper.name
+  size   = 1
+  region = "lhr"
 }
 
 resource "fly_ip" "zookeeper-ip" {

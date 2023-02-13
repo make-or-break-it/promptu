02cf4354c12a6ac4e3ae4b3e70aedbb24a476470
Sanyia Saidova <sanyia.saidova@deliveroo.co.uk>
2023-02-13 18:46:58 +0000

    Use promptu prefix for all apps and update toml to reflect correct app names

--- infrastructure/kafka.tf
+++ infrastructure/kafka.tf
@@ -1,11 +1,11 @@
 resource "fly_volume" "kafka" {
   name   = "kafka_data"
-  app    = fly_app.kafka.name
+  app    = fly_app.promptu-kafka.name
   size   = 1
   region = "lhr"
 }
 
 resource "fly_ip" "kafka-ip" {
-  app  = fly_app.kafka.name
+  app  = fly_app.promptu-kafka.name
   type = "v6"
 }

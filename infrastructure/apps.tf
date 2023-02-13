324ee08c9b6c6b99c2cb909158713bdc0ebbd486
Sanyia Saidova <47756528+sansaid@users.noreply.github.com>
2023-02-13 20:15:37 +0000

    Tidy up some bits (#68)
    * Renaming DB user to something more appropriate
    
    * Removing old apps directory that have been renamed
    
    * Use promptu prefix for all apps and update toml to reflect correct app names

--- infrastructure/apps.tf
+++ infrastructure/apps.tf
@@ -4,26 +4,26 @@ resource "fly_app" "promptu" {
 }
 
 resource "fly_app" "promptu-feeder-api" {
-  name = "promptu-feeder-api"
+  name = var.promptu_fly_io_name_suffix == "" ? "promptu-feeder-api" : "promptu-feeder-api-${var.promptu_fly_io_name_suffix}"
   org  = var.promptu_fly_io_org_name
 }
 
 resource "fly_app" "promptu-post-api" {
-  name = "promptu-post-api"
+  name = var.promptu_fly_io_name_suffix == "" ? "promptu-post-api" : "promptu-post-api-${var.promptu_fly_io_name_suffix}"
   org  = var.promptu_fly_io_org_name
 }
 
 resource "fly_app" "promptu-db-updater" {
-  name = "promptu-db-updater"
+  name = var.promptu_fly_io_name_suffix == "" ? "promptu-db-updater" : "promptu-db-updater-${var.promptu_fly_io_name_suffix}"
   org  = var.promptu_fly_io_org_name
 }
 
-resource "fly_app" "kafka" {
-  name = "kafka"
+resource "fly_app" "promptu-kafka" {
+  name = var.promptu_fly_io_name_suffix == "" ? "promptu-kafka" : "promptu-kafka-${var.promptu_fly_io_name_suffix}"
   org  = var.promptu_fly_io_org_name
 }
 
-resource "fly_app" "zookeeper" {
-  name = "zookeeper"
+resource "fly_app" "promptu-zookeeper" {
+  name = var.promptu_fly_io_name_suffix == "" ? "promptu-zookeeper" : "promptu-zookeeper-${var.promptu_fly_io_name_suffix}"
   org  = var.promptu_fly_io_org_name
 }
--- infrastructure/zookeeper.tf
+++ infrastructure/zookeeper.tf
@@ -1,11 +1,11 @@
 resource "fly_volume" "zookeeper" {
   name   = "zookeeper_data"
-  app    = fly_app.zookeeper.name
+  app    = fly_app.promptu-zookeeper.name
   size   = 1
   region = "lhr"
 }
 
 resource "fly_ip" "zookeeper-ip" {
-  app  = fly_app.zookeeper.name
+  app  = fly_app.promptu-zookeeper.name
   type = "v6"
 }

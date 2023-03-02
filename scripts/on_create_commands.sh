a8b7d72017511a115f49df5ee2fc04d928b670d1
Sanyia Saidova <47756528+sansaid@users.noreply.github.com>
2023-03-02 16:05:19 +0000

    Replace Homebrew mongosh installation with custom (#82)

---
+++ scripts/on_create_commands.sh
@@ -0,0 +1,8 @@
+#/usr/bin/env bash
+
+# Install mongosh
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-mongosh
